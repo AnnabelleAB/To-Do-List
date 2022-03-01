6ff11ac2a5ff796c5842013658e05852577e87ca
AnnabelleAB <sunhuaqing01@gmail.com>
2022-03-01 09:07:54 -0800

    web: npm start

--- app.js
+++ app.js
@@ -192,10 +192,6 @@ app.get("/about", function(req, res) {
   res.render("about");
 });
 
-let port = process.env.PORT;
-// if port == null || port == "") {
-//   port = 3000;
-// }
-app.listen(port || 3000, function() {
-  console.log("Server has started Successfully on dynamic port ");
+app.listen(process.env.PORT || 3000, function(req, res) {
+  console.log("Server started on port 3000");
 });
